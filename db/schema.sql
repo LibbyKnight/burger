1611b592eeceb545f627f7f3c6c93a7f42e0bd8d
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-01-26 18:29:17 -0500

    initial files and table creation

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+
+CREATE TABLE burgers (
+  id int AUTO_INCREMENT,
+  burger_name varchar(255) NOT NULL,
+  devoured boolean NOT NULL,
+  date timestamp NOT NULL,
+  PRIMARY KEY(id)
+);
+
+
+
+INSERT INTO burgers () VALUES ("");
+INSERT INTO burgers () VALUES ("");
+INSERT INTO burgers () VALUES ("");
+INSERT INTO burgers () VALUES ("");
+
+
---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require ("express");
+var methodOverride = require ("method-override");
+var bodyParser = require ("body-parser");
+
+
+module exports [orm]
+
+var orm = { insert function (),
+delete ""
+update ""
+
+	}
