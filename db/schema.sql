afe193bf142b04e9052f5e14f66b011142188a6c
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-01-27 13:01:07 -0500

    populated table with burgers

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,9 @@ USE burgers_db;
 
 CREATE TABLE burgers (
   id int AUTO_INCREMENT,
-  burger_name varchar(255) NOT NULL,
-  devoured boolean NOT NULL,
-  date timestamp NOT NULL,
+  `burger_name` varchar(255) NOT NULL,
+  `devoured` boolean DEFAULT FALSE,
+  `date` current_timestamp NOT NULL,
   PRIMARY KEY(id)
 );
-
-
-
-INSERT INTO burgers () VALUES ("");
-INSERT INTO burgers () VALUES ("");
-INSERT INTO burgers () VALUES ("");
-INSERT INTO burgers () VALUES ("");
-
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,19 +2,12 @@
 USE burgers_db;
 
 
-CREATE TABLE burgers (
-  id int AUTO_INCREMENT,
-  burger_name varchar(255) NOT NULL,
-  devoured boolean NOT NULL,
-  date timestamp NOT NULL,
-  PRIMARY KEY(id)
-);
 
 
 
-INSERT INTO burgers (burger_name) VALUES ("Portobello Burger");
-INSERT INTO burgers (burger_name) VALUES ("Swiss Truffle Burger");
-INSERT INTO burgers (burger_name) VALUES ("BBQ Bacon Burger");
-INSERT INTO burgers (burger_name) VALUES ("Mac n Cheese Burger");
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ("Portobello Burger", FALSE, current_timestamp);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ("Swiss Truffle Burger", FALSE, current_timestamp);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ("BBQ Bacon Burger", FALSE, current_timestamp);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ("Mac n Cheese Burger", FALSE, current_timestamp);
 
 
