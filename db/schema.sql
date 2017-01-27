6b050bb58515fb7d82e8c3b4a603904a2f14f1de
LibbyKnight <lopez.beatriz.s@gmail.com>
2017-01-27 18:19:06 -0500

    finished with mysql connection and on to config folder

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE burgers_db;
-USE burgers_db;
+USE `burgers_db`;
 
 
-CREATE TABLE burgers (
-  id int AUTO_INCREMENT,
+CREATE TABLE `burgers` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `burger_name` varchar(255) NOT NULL,
   `devoured` boolean DEFAULT FALSE,
-  `date` current_timestamp NOT NULL,
+  `date` timestamp DEFAULT current_timestamp NOT NULL,
   PRIMARY KEY(id)
 );
 
